8395b4cb669beb2e76623f82aded5d686467d0f7
Aneliya <aneliya_karim@yahoo.co.uk>
2023-03-28 14:56:08 +0200

    Initial Commit

---
+++ Greety/GreetyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetyApp.swift
+//  Greety
+//
+//  Created by Aneliya Mukhamedkarimova on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GreetyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
